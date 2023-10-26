6ba1be546383153b0ddbbf696774c735ea6db8dd
2431380yasamingol <yasamingol>
2023-10-26 06:32:21 -0700

    Create Menu class
    The overall class and basic functionalities have been implemented.

---
+++ menu.py
@@ -0,0 +1,24 @@
+from task_list import TaskList
+
+
+class Menu:
+    def __init__(self):
+        self.task_list = TaskList()
+
+    def display_main_menu(self):
+        return 0
+
+    def main_menu(self):
+        return 0
+
+    def add_task(self):
+        return 0
+
+    def remove_task(self):
+        return 0
+
+    def mark_task_completed(self):
+        return 0
+
+    def add_category(self):
+        return 0
