18589f055267cf8a02bc8573d985161cf4fc2552
yasmin.kadkhodaei <yasmin.kadkhodaei@gmail.com>
2023-10-26 17:03:00 +0330

    Add Task Class Functions

--- task.py
+++ task.py
@@ -3,3 +3,8 @@ class Task:
         self.description = description
         self.completed = False
         self.category = category
+
+    def toggle_completed(self):
+        return 0
+    def __str__(self):
+        return 0
