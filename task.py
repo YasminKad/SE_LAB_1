f6f83769c281548dec88729c96bc148a23e864e0
yasmin.kadkhodaei <yasmin.kadkhodaei@gmail.com>
2023-10-26 17:01:36 +0330

    Revert "Add Task Class Functions"
    This reverts commit 2b783f71b234223db75ce87bdb36a61a6f63cdbb.

--- task.py
+++ task.py
@@ -3,10 +3,3 @@ class Task:
         self.description = description
         self.completed = False
         self.category = category
-    def __init__(self, description, category):
-        return 0
-    def toggle_completed(self):
-        return 0
-
-    def __str__(self):
-        return 0
