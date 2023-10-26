898163201b1ad73968db14b3f298445f819d509d
2431380yasamingol <yasamingol>
2023-10-26 06:23:55 -0700

    Create TaskList class
    The overall class and basic functionalities have been implemented.

---
+++ task_list.py
@@ -0,0 +1,19 @@
+class TaskList:
+    def __init__(self):
+        self.tasks = []
+        self.categories = {}
+
+    def add_task(self, task):
+        return 0
+
+    def remove_task(self, task):
+        return 0
+
+    def show_tasks(self):
+        return 0
+
+    def add_category(self, name):
+        return 0
+
+    def show_categories(self):
+        return 0
